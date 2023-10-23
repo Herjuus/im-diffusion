e8a900b3135368fad9e2af6f86995ec6cf649514
elvinos <elvinos33@duck.com>
2023-10-23 22:20:32 +0200

    Image Optimization

--- src/components/ImageCard.svelte
+++ src/components/ImageCard.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+  import { Image } from "@unpic/svelte"
   export let prompt: string;
   export let image: any;
 
@@ -6,5 +7,5 @@
 
 <button class="card variant-ghost-surface p-5 m-5 text-start space-y-2 w-[350px] flex flex-col items-center">
   <span class="line-clamp-1 font-bold text-lg w-full">{prompt}</span>
-  <img class="rounded-md h-[300px] w-[300px] {!image && 'placeholder animate-pulse p-[0.51px]'}" src="{`data:image/png;base64,${image}`}" alt="">
+  <Image class="rounded-md h-[300px] w-[300px] {!image && 'placeholder animate-pulse p-[0.51px]'}" src="{`data:image/png;base64,${image}`}" alt=""/>
 </button>
