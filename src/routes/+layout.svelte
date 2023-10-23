d816270b52138ce79222c918ee7b62005d9b6002
Elvin Baloku <elvinos33@duck.com>
2023-10-23 14:06:24 +0200

    Lightswitch

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,9 +2,14 @@
 	import '../app.postcss';
 
 	// Floating UI for Popups
-	import { computePosition, autoUpdate, flip, shift, offset, arrow } from '@floating-ui/dom';
+  import { computePosition, autoUpdate, flip, shift, offset, arrow } from '@floating-ui/dom';
+  import { LightSwitch } from '@skeletonlabs/skeleton';
 	import { storePopup } from '@skeletonlabs/skeleton';
-	storePopup.set({ computePosition, autoUpdate, flip, shift, offset, arrow });
+  storePopup.set({ computePosition, autoUpdate, flip, shift, offset, arrow });
 </script>
+
+<div class="p-2">
+  <LightSwitch />
+</div>
 
 <slot />
