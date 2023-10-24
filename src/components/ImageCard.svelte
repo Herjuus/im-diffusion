8e2b0d1ad691d742ba860da567ef6ecc3f535026
Elvin Baloku <elvinos33@duck.com>
2023-10-24 07:59:16 +0200

    aspect ratio

--- src/components/ImageCard.svelte
+++ src/components/ImageCard.svelte
@@ -7,5 +7,5 @@
 
 <button class="card variant-ghost-surface p-5 m-5 text-start space-y-2 w-[350px] flex flex-col items-center">
   <span class="line-clamp-1 font-bold text-lg w-full">{prompt}</span>
-  <Image class="rounded-md h-[300px] w-[300px] {!image && 'placeholder animate-pulse p-[0.51px]'}" src="{`data:image/png;base64,${image}`}" alt=""/>
+  <Image height={500} width={500} class="rounded-md min-h-[300px] min-w-[300px] {!image && 'placeholder animate-pulse p-[1px]'}" src="{`data:image/png;base64,${image}`}" alt=""/>
 </button>
